0082ec4d15fc0017f3902ad1ad58f30867f2e405
adamnfish <adamnfish@gmail.com>
2025-03-20 22:27:55 +0000

    Scalafmt

--- src/main/scala/io/adamnfish/pcb/Exif.scala
+++ src/main/scala/io/adamnfish/pcb/Exif.scala
@@ -2,33 +2,45 @@ package io.adamnfish.pcb
 
 import org.apache.commons.imaging.Imaging
 import org.apache.commons.imaging.formats.jpeg.JpegImageMetadata
-import org.apache.commons.imaging.formats.tiff.constants.{ExifTagConstants, TiffDirectoryType}
+import org.apache.commons.imaging.formats.tiff.constants.{
+  ExifTagConstants,
+  TiffDirectoryType
+}
 import org.apache.commons.imaging.formats.tiff.taginfos.TagInfoAscii
 
 import java.io.File
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 
-
-/**
- * Not currently used, but this would be useful for more advanced photo
- * datetime logic.
- *
- * e.g. using 4am as a the date boundary rather than midnight, to leave
- * nighttime activities together.
- */
+/** Not currently used, but this would be useful for more advanced photo
+  * datetime logic.
+  *
+  * e.g. using 4am as a the date boundary rather than midnight, to leave
+  * nighttime activities together.
+  */
 object Exif {
-  val tzOffsetTagInfo = new TagInfoAscii("OffsetTimeOriginal", 0x9011, 8, TiffDirectoryType.EXIF_DIRECTORY_EXIF_IFD)
+  val tzOffsetTagInfo = new TagInfoAscii(
+    "OffsetTimeOriginal",
+    0x9011,
+    8,
+    TiffDirectoryType.EXIF_DIRECTORY_EXIF_IFD
+  )
 
   def readPixelDate(file: File): Either[String, ZonedDateTime] = {
     val m = Imaging.getMetadata(file)
     m match {
       case jpegMetadata: JpegImageMetadata =>
-        val dateField = jpegMetadata.getExif.findField(ExifTagConstants.EXIF_TAG_DATE_TIME_ORIGINAL)
+        val dateField = jpegMetadata.getExif.findField(
+          ExifTagConstants.EXIF_TAG_DATE_TIME_ORIGINAL
+        )
         val tzOffsetField = jpegMetadata.getExif.findField(tzOffsetTagInfo)
-        val dateTimeStr = s"${dateField.getValue.asInstanceOf[String]}${tzOffsetField.getValue.asInstanceOf[String]}"
+        val dateTimeStr =
+          s"${dateField.getValue.asInstanceOf[String]}${tzOffsetField.getValue.asInstanceOf[String]}"
         println(dateTimeStr)
-        val imageTime = ZonedDateTime.parse(dateTimeStr, DateTimeFormatter.ofPattern("yyyy:MM:dd HH:mm:ssxxx"))
+        val imageTime = ZonedDateTime.parse(
+          dateTimeStr,
+          DateTimeFormatter.ofPattern("yyyy:MM:dd HH:mm:ssxxx")
+        )
         println(imageTime)
         Right(imageTime)
       case _ =>
