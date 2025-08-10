aaaacbc1668a3fde0ed28e303e189515d4562e9c
adamnfish <adamnfish@gmail.com>
2025-08-10 12:36:05 +0100

    Tidy up models

--- src/main/scala/io/adamnfish/pcb/Output.scala
+++ src/main/scala/io/adamnfish/pcb/Output.scala
@@ -13,24 +13,23 @@ object ConsoleOutput extends Output:
 
 // Test functionality
 
-class TestOutput extends Output {
+class TestOutput extends Output:
   private val output = scala.collection.mutable.Buffer[OutputLine]()
   def stdout(line: String): Unit = output += OutputLine.Stdout(line)
   def stderr(line: String): Unit = output += OutputLine.Stderr(line)
-  
+
   def getAllOutput: List[OutputLine] = output.toList
-  def getStdout: List[String] = output.collect {
-    case OutputLine.Stdout(line) => line
+  def getStdout: List[String] = output.collect { case OutputLine.Stdout(line) =>
+    line
   }.toList
-  def getStderr: List[String] = output.collect {
-    case OutputLine.Stderr(line) => line
+  def getStderr: List[String] = output.collect { case OutputLine.Stderr(line) =>
+    line
   }.toList
   def clear(): Unit = output.clear()
-}
 
-enum OutputLine(val content: String):
-  case Stdout(override val content: String) extends OutputLine(content)
-  case Stderr(override val content: String) extends OutputLine(content)
+enum OutputLine:
+  case Stdout(content: String) extends OutputLine
+  case Stderr(content: String) extends OutputLine
 object OutputLine:
   def out(s: String): OutputLine = Stdout(s)
   def err(s: String): OutputLine = Stderr(s)
